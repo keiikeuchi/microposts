de43b0342969c9d59b13e989ed08203cf06979f7
keiikeuchi <giggsister1996@gmail.com>
2016-06-02 01:15:06 +0000

    goodness

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-
+ 
+  # before_actionを書く順序が大切です。set_userしてからでないと
+  # set_loginメソッド内で@userが空になってしまいます。
+  before_action :set_user, only: [:show, :edit, :update]
+  # ↑showアクションも同じコードがあるので追加しました。
+  before_action :set_login, only: [:edit, :update]
+  
+  
   def show 
-   @user = User.find(params[:id])
   end
   
   def new
@@ -17,11 +23,36 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+   
+  end
+  
+  def update
+    if @user.update(user_params)
+     redirect_to user_path(current_user) , notice: 'ユーザーを編集しました'
+    else
+     render 'edit'
+    end
+  end
+  
+
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                               :password_confirmation,
+                               :profile, :area)
+    # ストロングパラメータにprofileとareaも追加しなければなりません。
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def set_login
+    redirect_to root_url if @user != current_user  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'signup',  to: 'users#new'
-
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'users/:id/edit', to: 'users#edit'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
