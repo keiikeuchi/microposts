e85967e6f25cab753dbf74d0f108d813151bca14
keiikeuchi <giggsister1996@gmail.com>
2016-06-03 12:14:35 +0000

    eee

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'users/:id/edit', to: 'users#edit'
+  
  
   
   resources :users do
